2b81af815367d69ddfd2a4b4b360591daeefa470
tatiespi <tatiana.espinosa@uao.edu.co>
2023-05-27 11:02:47 -0500

    Challenge 18

--- Controllers/Task.js
+++ Controllers/Task.js
@@ -36,10 +36,72 @@ const listarTasks = async (req, res = express.request) => {
   }
 };
 
+const actualizarTask = async (req, res = express.request) => {
+    const taskId = req.params.id;
+    const taskBody = req.body;
+    try {
+      const task = await Task.findById(taskId);
+      if (!task) {
+        return res.status(404).json({
+          ok: false,
+          task: "Task not found",
+        });
+      }
+      if (!task.user.equals(new ObjectId(req.uid))) {
+        return res.status(500).json({
+          ok: false,
+          task: "El usuario no tiene permisos para modificar esta task",
+        });
+      }
+      const updatedTask = await Task.findByIdAndUpdate(taskId, taskBody, {
+        new: true,
+      });
+      res.status(200).json({
+        ok: true,
+        updatedTask,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        ok: false,
+        msg: "Error Interno",
+      });
+    }
+  };
+  
+  const eliminarTask = async (req, res = express.request) => {
+    const taskId = req.params.id;
+    const taskBody = req.body;
+    try {
+      const task = await Task.findById(taskId);
+      if (!task.user.equals(new ObjectId(req.uid))) {
+        return res.status(500).json({
+          ok: false,
+          task: "El usuario no tiene permisos para eliminar esta task",
+        });
+      }
+      const updatedTask = await Task.findByIdAndDelete(taskId, taskBody, {
+        new: true,
+      });
+      res.status(200).json({
+        ok: true,
+        updatedTask,
+      });
+      console.log(`Task ${taskId} deleted`);
+    } catch (error) {
+      console.log(error);
+      return res.status(404).json({
+        ok: false,
+        task: "Task not found",
+      });
+    }
+  };
 
 
 module.exports = {
   listarTasks,
   crearTask,
+  actualizarTask,
+  eliminarTask,
   
 };
